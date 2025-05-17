587cdccbc58c42d5ecca00f1ccf07604ffde4515
Som <132274403+Sompalkar@users.noreply.github.com>
2025-05-17 22:12:15 +0530

    Update index

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,12 @@ const port = process.env.PORT || 5000
 const prisma = new PrismaClient()
 
 // Middleware
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  }),
+)
 app.use(express.json())
 
 // Routes
